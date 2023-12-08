ade62790f376750c1b10bf7e04eadfa22ad92ff3
bdas615 <bdas@brandinstitute.com>
2023-12-08 21:23:18 +0530

    basic html built for app

---
+++ quizAppLogic.js
@@ -0,0 +1,31 @@
+const question = [
+    {
+        questions: "What is the largest animal in the world",
+        answers: [
+            { text : "Shark" , correct : false },
+            { text : "Blue Whale" , correct : true },
+            { text : "Elephant" , correct : false },
+            { text : "Hippo" , correct : false },
+        ]
+    },
+    {
+        questions : "What is the biggest planet in the solar system",
+        answers : [
+            { text: "Mars", correct: false },
+            { text: "Jupiter", correct: true },
+            { text: "Neptune", correct: false },
+            { text: "Venus", correct: false },
+        ]
+    },
+    {
+        questions : "What is the largest ocean in the world",
+        answers : [
+            { text: "Indian Ocean", correct: false },
+            { text: "Pacific Ocean", correct: true },
+            { text: "Artic ocean", correct: false },
+            { text: "Atlantic Ocean", correct: false },
+        ]
+    },
+];
+
+
