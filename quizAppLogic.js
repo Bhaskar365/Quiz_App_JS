3c7c754b0197f232cb6dd6d614f4298fa790df5b
bdas615 <bdas@brandinstitute.com>
2023-12-12 17:12:12 +0530

    quiz app done

--- quizAppLogic.js
+++ quizAppLogic.js
@@ -1,6 +1,6 @@
-const question = [
+const questions = [
     {
-        questions: "What is the largest animal in the world",
+        question: "What is the largest animal in the world",
         answers: [
             { text : "Shark" , correct : false },
             { text : "Blue Whale" , correct : true },
@@ -9,7 +9,7 @@ const question = [
         ]
     },
     {
-        questions : "What is the biggest planet in the solar system",
+        question : "What is the biggest planet in the solar system",
         answers : [
             { text: "Mars", correct: false },
             { text: "Jupiter", correct: true },
@@ -18,7 +18,7 @@ const question = [
         ]
     },
     {
-        questions : "What is the largest ocean in the world",
+        question : "What is the largest ocean in the world",
         answers : [
             { text: "Indian Ocean", correct: false },
             { text: "Pacific Ocean", correct: true },
@@ -28,4 +28,91 @@ const question = [
     },
 ];
 
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-btn');
 
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex+1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer=>{
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click',selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button=>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    }
+    else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    }
+    else {
+        startQuiz();
+    }
+});
+
+
+
+
+startQuiz();
